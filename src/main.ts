bee641ffbba77dd4e645a8f82174ae12aaa796c3
chachoblow <wesleyr.klein@gmail.com>
2022-09-04 09:05:19 -0700

    Add google analytics

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,16 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import pinia from "./stores";
 import router from "./router";
+import VueGtag from "vue-gtag";
 
 const app = createApp(App);
 
 app.use(pinia);
 app.use(router);
+app.use(VueGtag, {
+    appName: 'Personal website',
+    pageTrackerScreenviewEnabled: true,
+    config: { id: "G-BDCX2RTVRT" }
+}, router);
 
 app.mount("#app");
